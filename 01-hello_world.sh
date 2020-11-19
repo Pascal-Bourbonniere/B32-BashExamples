e6d2b53465e3ed90ef9e3ef389ebbebe15e11532
Pascal <pascboup@gmail.com>
2020-11-19 15:32:55 -0500

    echo-1 termine

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+nom="Pascal"
+echo  "Pascal Bourbonniere"
+
+echo -n "Vive"
+echo "Linux"
+
+# va ecrire Vive Linux sans saut de ligne
+
+echo -e "1\n2\n3\n4" #-e interprete les metacaracteres
+
+read -p "My name is?" nom
+#read c'est comme un cin
+#, -p "mon message"
+# varaiable_qui_recoit _la valeur, ici nom
+
+echo "thic ka thick la tchic" $nom
