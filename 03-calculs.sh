814f27e72a14f8dc9f712e298156e6b89ad1b2d3
Pascal <pascboup@gmail.com>
2020-11-19 16:30:11 -0500

    calculs-03

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+echo 10+10 #affiche 10+10
+echo $((10+10))
+let resultat=10*10 #si on ecrit  let devant la variable qu'on assigne,
+		   #agit comme un $(())
+echo $resultat 
+
+echo "le resultat de $1 -$2 = $(($1+$2))"
 
