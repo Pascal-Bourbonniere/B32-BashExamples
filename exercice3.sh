34995759a483f98cd1912a8efdd739ed98a4fbae
Pascal <pascboup@gmail.com>
2020-11-20 11:39:40 -0500

    fin

--- exercice3.sh
+++ exercice3.sh
@@ -33,6 +33,32 @@ echo "       @[#########]"
 sleep .5
 clear
 
-echo "       ***BOOOM****"
+echo "      ****BOOOM****"
+sleep 2
+clear
+
+
+faireEspace()
+{
+j=0
+for ((i=0; i<$1;i++))
+do
+echo -n " "
+done
+}
+
+max=30
+
+for ((i=0;i<$max;i++))
+do
+	clear
+	faireEspace $i
+	echo "@"
+	sleep .2
+done
+
+clear
+faireEspace $max
+ echo "********BOOOOOOOOOOOM*********"
 sleep 2
 clear
